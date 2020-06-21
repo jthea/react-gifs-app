739d94871247c2f9ffec8deb4f9adf34dfe7a51e
jthea <jthea0609@gmail.com>
2020-06-21 13:50:22 -0500

    Added docs

---
+++ docs/precache-manifest.d5a3a78561901acf93c38b73350e0d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477ec7102bca91a7bee9d2e3d7e71028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f8f094f5d21e77dd7f",
+    "url": "/static/css/main.03a96cbb.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f8f094f5d21e77dd7f",
+    "url": "/static/js/main.40479ba3.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
